8ba70655b60a1809a8c07d1080ff1abf4562e8f2
Williams <cewilliams@lm-sjn-21020642.lan>
2018-09-22 09:32:10 -0700

    Remove unused aliases

--- init.sh
+++ init.sh
@@ -17,32 +17,6 @@ alias sr="screen -r"
 alias ll="ls -alF"
 alias la="ls -A"
 alias l="ls -CF"
-alias gr="egrep -r"
-alias brc="vim ~/src/bashrc/init.sh"
-alias brcl="vim ~/src/bashrc/init-local.sh"
-alias src="source ~/.bashrc"
-alias k="kill %%"
-alias r="bundle exec rspec"
-alias n="npm run start"
-alias nt="npm test"
-alias beg="bundle exec guard"
-# Git.
-alias gca="git commit -a"
-alias gb="git branch"
-alias gd="git diff"
-alias gdc="git diff --cached"
-alias gch="git checkout"
-alias gpom="git pull origin master"
-alias gp="git pull"
-alias gs="git status"
-alias gbddd="git branch --merged | grep -v '\*' | grep -v '^  master$' | xargs -n 1 git branch -d"
-# Common repos.
-alias s="cd ~/src"
-alias sgo="cd ~/src/go/src"
-alias sb="cd ~/src/bashrc"
-# Misc.
-alias dc="docker-compose"
-alias ember="./node_modules/ember-cli/bin/ember"
 
 # Eternal bash history.
 # ---------------------
@@ -63,16 +37,6 @@ stophistory () {
   PROMPT_COMMAND="bash_prompt_command"
   echo 'History recording stopped. Make sure to `kill -9 $$` at the end of the session.'
 }
-
-# Go path.
-export GOPATH=$HOME/src/go/
-export PATH=$PATH:$GOPATH/bin
-
-# Setup ssh-agent.
-if [[ $OSTYPE == 'linux-gnu' ]]; then
-  pgrep -f ssh-agent > /dev/null || ssh-agent > ~/.ssh/agent_config.sh
-  eval `cat ~/.ssh/agent_config.sh`
-fi
 
 # Enable tab completion for aliases. Must come after everything else.
 source $DIR/alias-tab-completion.sh
